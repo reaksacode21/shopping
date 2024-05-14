226a1d295e6d2a6cf7e82454533970bb1de988cb
reaksacode21 <reaksara497@gmail.com>
2024-05-14 19:43:53 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="container">
+     <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { FaAlignJustify } from "react-icons/fa";
+import './Navbarcss.css'
+
+function Navbar() {
+    return (
+        <>
+            <nav>
+                <input type="checkbox" id="check"/>
+                <label for="check" class="checkbtn">
+                    <i class="fas fa-bars">
+
+                        <FaAlignJustify />
+                    </i>
+                </label>
+                <label class="logo">LiveNew</label>
+                <ul>
+                    <li><a class="active" href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Feedback</a></li>
+                </ul>
+            </nav>
+            <section></section>
+        </>
+    )
+}
+
+export default Navbar
