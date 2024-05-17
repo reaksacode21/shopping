4211bc5b2182ee410fe5698d6c6d7c4be5069e56
reaksacode21 <reaksara497@gmail.com>
2024-05-17 22:19:26 +0700

    this for updata file

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -1,14 +1,24 @@
 import React from 'react'
 
 function Detail() {
-  return (
-    <div className='contianer'>
-        <div className='row'>
-            
+    return (
+        <div className=" contianer bg-danger">
+            <div class="row">
+                <div class="col-sm-3">
+                  <h1>sa</h1>
+                </div>
+                <div class="col-sm-3">
+                   <h1>sa</h1>
+                </div>
+                <div class="col-sm-3">
+                    <h1>sa</h1>
+                </div>
+                <div class="col-sm-3">
+                    <h1>sa</h1>
+                </div>
+            </div>
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default Detail
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import CardHome from '../Components/CardHome'
 import Slider from '../Components/Slider'
+import Detail from '../Components/Detail'
 function HomePage() {
   return (
     <div className='container'>
        <div className='slider'>
        <Slider/>
+       </div>
+       <div className='detail'>
+      <Detail/>
        </div>
         {/* <h1>This Is Shopping</h1> */}
         <CardHome/>
