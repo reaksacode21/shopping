c747b5543b99a488d64910fa9dd222d8a768695d
reaksaracode21 <reaksara497@gmail.com>
2024-05-21 14:36:31 +0700

    update card

--- src/Components/ProductDetail.js
+++ src/Components/ProductDetail.js
@@ -5,10 +5,10 @@ function ProductDetail() {
         <div className='all-detail'>
             <div className="row  all-detailReal">
                 <div className="col-sm-12 col-md-6 col-lg-6 all-productPic">
-                <h1>sa</h1>
+                <h1>Product Picture</h1>
                 </div>
                 <div className="col-sm-12 col-md-6 col-lg-6 all-productDetail">
-                <h1>sa</h1>
+                <h1>Product Details</h1>
                 </div>
             </div>
         </div>
